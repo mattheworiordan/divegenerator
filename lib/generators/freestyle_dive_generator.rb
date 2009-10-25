366b701a29c9c68371436632f4d10ea4c5e2b51d
Matthew O'Riordan <matthew@Matt-MacbookPro.local>
2009-10-25 11:53:02 +0000

    General clean up of old files

--- lib/generators/freestyle_dive_generator.rb
+++
@@ -1,9 +0,0 @@
-module Generators
-  class FreestyleDiveGenerator < DiveGenerator
-    def initialize
-      super
-      ('1'..'10').each { |v| @movePool[v] = SkydiveMove.new(1, DIVE_RANDOM, v) }
-      @minPointsPerRound = 5
-    end
-  end
-end
