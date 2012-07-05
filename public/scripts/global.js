6412bd9d9386b2fbe47f56af8ce49c0300ed4ad0
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-07-05 15:49:10 +0200

    Bug fix: Access the symbol object of the data returned

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -1,101 +1,101 @@
 function generate_dives()
 {
-	var originaltext = $('#generate_dives_button').val(); // get the button text
-	
-	// hide dive results and show user we are now loading
-	$('#diveresult').css("display", "none");
-	$('#generate_dives_button').val("Loading...");
-	
-	// check that all params have valid selections and build up params to pass through on JSON request to app
-	urlparams = "";
-	paramdic = {'post[discipline]':'Discipline','post[jumps]':'Jumps','post[sequence]':'Dive sequence'};
-	for (field in paramdic) {
-		paramval = $("input[name='" + field + "']:checked").val();
-		if (!paramval) {
-			alert ('Please select an option for "' + paramdic[field] + '"');
-			return;
-		}
-		urlparams += (urlparams.length ? "&" : "") + escape(field) + "=" + escape(paramval);
-	}
-	
-	// send request
-	$.getJSON("/shortest_path.json?" + urlparams, function(data, textStatus) {
-	  	if (textStatus != "success" || !data.success) {
-			alert ("Unfortunately your dives could not be generated: \n" + data.message + "\nServer response: " + textStatus)
-		} else {
-			// clear out old dive pool
-			(dp = $('#divepool')).empty();
-			var sequence = 1;
-			for (jump in data.data) {
-				var moveSymbol = [];
-				for (move in data.data[jump])
-					 moveSymbol.push (data.data[jump][move][2])
-				dp.append ("<div class='dive'><div class='number'>" + sequence++ + ". </div><div class='sequence'>" + moveSymbol.join(' - ') + "</div></div>");
-			}
-			$('#diveresult #summary').text ("Has a total of " + (sequence-1) + " dives.");
-		}
-		$('#diveresult').css("display", "block");
-		$('#generate_dives_button').val(originaltext);
-	});
-	
-	$('#diveresult #download a').attr ("href", "/shortest_path.csv?" + urlparams);
+  var originaltext = $('#generate_dives_button').val(); // get the button text
+
+  // hide dive results and show user we are now loading
+  $('#diveresult').css("display", "none");
+  $('#generate_dives_button').val("Loading...");
+
+  // check that all params have valid selections and build up params to pass through on JSON request to app
+  urlparams = "";
+  paramdic = {'post[discipline]':'Discipline','post[jumps]':'Jumps','post[sequence]':'Dive sequence'};
+  for (field in paramdic) {
+    paramval = $("input[name='" + field + "']:checked").val();
+    if (!paramval) {
+      alert ('Please select an option for "' + paramdic[field] + '"');
+      return;
+    }
+    urlparams += (urlparams.length ? "&" : "") + escape(field) + "=" + escape(paramval);
+  }
+
+  // send request
+  $.getJSON("/shortest_path.json?" + urlparams, function(data, textStatus) {
+      if (textStatus != "success" || !data.success) {
+      alert ("Unfortunately your dives could not be generated: \n" + data.message + "\nServer response: " + textStatus)
+    } else {
+      // clear out old dive pool
+      (dp = $('#divepool')).empty();
+      var sequence = 1;
+      for (jump in data.data) {
+        var moveSymbol = [];
+        for (move in data.data[jump])
+           moveSymbol.push (data.data[jump][move].symbol)
+        dp.append ("<div class='dive'><div class='number'>" + sequence++ + ". </div><div class='sequence'>" + moveSymbol.join(' - ') + "</div></div>");
+      }
+      $('#diveresult #summary').text ("Has a total of " + (sequence-1) + " dives.");
+    }
+    $('#diveresult').css("display", "block");
+    $('#generate_dives_button').val(originaltext);
+  });
+
+  $('#diveresult #download a').attr ("href", "/shortest_path.csv?" + urlparams);
 }
 
 var jump_max = 40, jump_all_text;
 
 function review_discipline()
 {
-	$("input[name='post[sequence]']").removeAttr ("disabled");
-	$("div.column#sequence").removeClass ("disabled");
-	$("input[name='post[sequence]']:checked").click();
+  $("input[name='post[sequence]']").removeAttr ("disabled");
+  $("div.column#sequence").removeClass ("disabled");
+  $("input[name='post[sequence]']:checked").click();
 }
 
 function review_jump_numbers()
 {
-	// now that we have selected dive sequence, enable jumps section
-	$("input[name='post[jumps]']").removeAttr ("disabled");
-	$("div.column#jumps").removeClass ("disabled");
-	
-	if ($(this).val() == "random") 	// if the user has selected random, then the all option is not relevant so offer 40 jumps
-	{
-		$("input[name='post[jumps]']").removeAttr ("disabled");
-		$("div.column#jumps label").removeClass ("disabled")
-		
-		jump_field.val('40');
-		$("label[for='" + jump_field.attr("id") + "']").text ("40");
-			
-	} else { // user has selected shortest route 
-		// disable all options other than ALL
-		$("input[name='post[jumps]']").attr ("disabled","true");
-		$("div.column#jumps label").addClass ("disabled")
-		
-		// set the ALL option to all
-		jump_field.removeAttr ("disabled");
-		jump_field.val('all');
-		
-		// set the text and enable the label for ALL option
-		$("label[for='" + jump_field.attr("id") + "']").text (jump_all_text);
-		$("label[for='" + jump_field.attr("id") + "']").removeClass ("disabled");
-		jump_field.attr ("checked", "true");
-	}
+  // now that we have selected dive sequence, enable jumps section
+  $("input[name='post[jumps]']").removeAttr ("disabled");
+  $("div.column#jumps").removeClass ("disabled");
+
+  if ($(this).val() == "random")  // if the user has selected random, then the all option is not relevant so offer 40 jumps
+  {
+    $("input[name='post[jumps]']").removeAttr ("disabled");
+    $("div.column#jumps label").removeClass ("disabled")
+
+    jump_field.val('40');
+    $("label[for='" + jump_field.attr("id") + "']").text ("40");
+
+  } else { // user has selected shortest route
+    // disable all options other than ALL
+    $("input[name='post[jumps]']").attr ("disabled","true");
+    $("div.column#jumps label").addClass ("disabled")
+
+    // set the ALL option to all
+    jump_field.removeAttr ("disabled");
+    jump_field.val('all');
+
+    // set the text and enable the label for ALL option
+    $("label[for='" + jump_field.attr("id") + "']").text (jump_all_text);
+    $("label[for='" + jump_field.attr("id") + "']").removeClass ("disabled");
+    jump_field.attr ("checked", "true");
+  }
 }
 
 $(document).ready(function(){
-	$.sifr({
-		path: '/fonts/',
-		save: true,
-		version: 2
-	});
-	$('h1').sifr({ font: 'sansbetween' });
-	
-	$('#generate_dives_button').click(generate_dives);
-	$("input[name='post[discipline]']").click(review_discipline);
-	$("input[name='post[sequence]']").click(review_jump_numbers);
-	
-	jump_field = $("input[name='post[jumps]']:last");
-	jump_all_text = $("label[for='" + jump_field.attr("id") + "']").text(); // get the text used to describe all from the app
-	
-	$("input[name='post[discipline]']:checked").click();
+  $.sifr({
+    path: '/fonts/',
+    save: true,
+    version: 2
+  });
+  $('h1').sifr({ font: 'sansbetween' });
+
+  $('#generate_dives_button').click(generate_dives);
+  $("input[name='post[discipline]']").click(review_discipline);
+  $("input[name='post[sequence]']").click(review_jump_numbers);
+
+  jump_field = $("input[name='post[jumps]']:last");
+  jump_all_text = $("label[for='" + jump_field.attr("id") + "']").text(); // get the text used to describe all from the app
+
+  $("input[name='post[discipline]']:checked").click();
 });
 
 
